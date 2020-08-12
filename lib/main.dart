5f33ee0c7d36e85d668297e5b17fd9b56541c846
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-08-12 18:15:51 +0530

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:eleventh_hour/screens/SplashScreen.dart';
+import 'package:eleventh_hour/screens/connectionLost.dart';
 import 'package:eleventh_hour/utilities/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'models/routeGenerator.dart';
 
@@ -8,9 +13,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+final GlobalKey<NavigatorState> nav = GlobalKey<NavigatorState>();
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamSubscription connectivitySubscription;
+
+  ConnectivityResult _previousResult;
+  @override
+  void dispose() {
+    super.dispose();
+    connectivitySubscription.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    connectivitySubscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult connectivityResult) {
+      if (connectivityResult == ConnectivityResult.none) {
+        nav.currentState.push(
+            MaterialPageRoute(builder: (BuildContext _) => ConnectionLost()));
+      } else if (_previousResult == ConnectivityResult.none) {
+        nav.currentState.push(
+            MaterialPageRoute(builder: (BuildContext _) => SplashScreen()));
+      }
+      _previousResult = connectivityResult;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
       theme: kDefaultTheme,
       debugShowCheckedModeBanner: false,
