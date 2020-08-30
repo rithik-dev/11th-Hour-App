2886437df3b35f588b8c0cb258d427cd67c84a24
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-08-30 15:19:52 +0530

    - Payments BABy

--- lib/views/Cart.dart
+++ lib/views/Cart.dart
@@ -2,10 +2,75 @@ import 'package:eleventh_hour/components/404Card.dart';
 import 'package:eleventh_hour/controllers/UserController.dart';
 import 'package:eleventh_hour/models/User.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   static const id = '/cart';
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  void handlerPaymentSuccess(PaymentSuccessResponse response) {
+    print("Payment success");
+    Fluttertoast.showToast(msg: "Payment Success");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    razorPay.clear();
+  }
+
+  static const platform = const MethodChannel("razorpay_flutter");
+  void handlerErrorFailure(PaymentFailureResponse response) {
+    print("Payment error");
+    Fluttertoast.showToast(msg: "Payment Error");
+  }
+
+  void handlerExternalWallet(ExternalWalletResponse response) {
+    print("External Wallet");
+    Fluttertoast.showToast(msg: "External");
+  }
+
+  Razorpay razorPay;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    razorPay = new Razorpay();
+
+    razorPay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+    razorPay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
+    razorPay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_NwfgCE8CdhXpFT",
+      "amount": num.parse("200") * 100,
+      "name": "11th Hour",
+      "description": "Start Learning!!!",
+      "prefill": {
+        "contact": "9953798220",
+        "email": "shivamthegreat.sv@gmail.com"
+      },
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+
+    try {
+      razorPay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +83,7 @@ class CartScreen extends StatelessWidget {
               Provider.of<User>(context, listen: false)
                   .updateUserInProvider(newUser);
             },
-            child: (user.cart == null || user.cart.length == 0)
+            child: !(user.cart == null || user.cart.length == 0)
                 ? Card404(title: "CART")
                 : Column(
                     children: <Widget>[
@@ -29,6 +94,12 @@ class CartScreen extends StatelessWidget {
                           ),
                         ),
                       ),
+                      FlatButton(
+                        child: Text("lol"),
+                        onPressed: () {
+                          openCheckout();
+                        },
+                      )
                     ],
                   ),
           );
