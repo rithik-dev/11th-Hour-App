203a17594e8df22df535d4e132eab777b8e91034
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-09-15 00:35:39 +0530

    - added lottie on search page

--- lib/components/NoSearchResults.dart
+++ lib/components/NoSearchResults.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class NoSearchResults extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("no search result"),
+    return Expanded(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Lottie.asset('assets/lottie/no-search-results.json'),
+        ],
+      ),
     );
   }
 }
--- lib/components/NoSearchText.dart
+++ lib/components/NoSearchText.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class NoSearchText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("noo search text"),
+    return Expanded(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Lottie.asset('assets/lottie/no-search-text.json'),
+          Text(
+            "\n\nSearch for new courses",
+            style: Theme.of(context).textTheme.headline1,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
